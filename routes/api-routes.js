1b1529452ae3540b10cf601263dc74eab2708ebc
Jordan Albers <albersjordan13@gmail.com>
2020-05-01 18:57:46 -0700

    removed files from develop folder and finished api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const fs = require('fs');
+let notes = [];
+
+router.get('/api/notes', function(req, res) {
+	fs.readFile('db/db.json', 'utf8', function(err, data) {
+		notes = JSON.parse(data);
+
+		console.log(notes);
+		return res.json(notes);
+	});
+});
+
+router.post('/api/notes', function(req, res) {
+	let newNote = req.body;
+	console.log('newnote', newNote);
+
+	
+	let id = 1;
+	if (notes.length !== 0) {
+		id = notes[notes.length - 1].id + 1;
+	}
+
+	newNote = { id, ...newNote };
+	notes.push(newNote);
+
+	fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+		console.log('file written successfully');
+
+		res.json(newNote);
+	});
+});
+router.delete('/api/notes/:id', function(req, res) {
+	let id = parseInt(req.params.id);
+	console.log('delete id', id);
+	let temp = notes.filter((elem) => elem.id !== id);
+
+	console.log(temp);
+	notes = temp;
+	fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+		console.log('file deleted');
+
+		res.json(notes);
+	});
+});
+
+router.put('/api/notes/:id', function(req, res) {
+	console.log(req.params);
+	let id = parseInt(req.params.id);
+	console.log('update id', id);
+	let temp = notes.map((elem) => {
+		if (elem.id === id) {
+			elem.title = req.body.title;
+			elem.text = req.body.text;
+		}
+		return elem;
+	});
+
+	
+	notes = temp;
+
+	fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+		console.log('you file was written');
+
+		res.json(notes);
+	});
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+app.listen(PORT, function() {
+	console.log('App listening on PORT ' + PORT);
+});
