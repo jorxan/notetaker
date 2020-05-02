34514367ffececd7ed9fe0c175c1f8347fc164e0
Jordan Albers <jordanalbers@Jordans-MacBook-Air.local>
2020-05-02 03:26:22 -0700

    fixed error on html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,12 +5,4 @@ router.get('/notes', function(req, res) {
 	res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-<<<<<<< HEAD
   module.exports= router
-=======
-router.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-module.exports = router;
->>>>>>> 98532d6879a5f0df4c118efc4b9d1537724e3739
