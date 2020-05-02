70d8279e944c96d8b063470144808b1ad1fb4ce1
Jordan Albers <jordanalbers@Jordans-MacBook-Air.local>
2020-05-02 03:17:39 -0700

    fixing delete problem

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const express = require("express");
+const apiRoutes = require("./routes/api-routes")
+const htmlRoutes = require("./routes/html-routes")
+    
+
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static("public"))
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+app.use(apiRoutes)
+app.use(htmlRoutes)
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
